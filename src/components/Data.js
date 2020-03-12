86d988e802c7d3c2cac31e799c4d7d81cf47eab4
Christopher Dougherty <christophertdoc@gmail>
2020-03-11 20:20:16 -0400

    cleanup

--- src/components/Data.js
+++ src/components/Data.js
@@ -3,42 +3,12 @@ import { useQuery } from "@apollo/react-hooks"
 import { gql } from "apollo-boost"
 import { withApollo } from "@apollo/react-hoc"
 
-// export const USERS = gql`
-//   {  
-//     users {
-//       id 
-//       name
-//     }
-//   }  
-// `
-
-// function Data() {
-
-//     const { loading, error, data } = useQuery(USERS)
-
-//     if (loading) return "Loading..."
-//     if (error) return `Error! ${error.message}`
-
-//     return (
-//         <div>
-//             {data.users.map(user => (
-//                 <h2 key={user.id}>{user.name}</h2>
-//             ))}
-//         </div>
-//     )
-// }
-
-// export default withApollo(Data)
-
-
-export const TODOS = gql`
-  {  
-    todos {
-      id 
-      title
-    }
-  }  
-`
+export const TODOS = gql`{  
+	todos {
+		id 
+		title
+	}
+}`
 
 function Data() {
 
