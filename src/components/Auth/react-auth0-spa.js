49ff8039eeedc947fe859078ca1f6eeb397f24fc
Christopher Dougherty <christophertdoc@gmail>
2020-03-11 20:48:24 -0400

    cleanup

--- src/components/Auth/react-auth0-spa.js
+++ src/components/Auth/react-auth0-spa.js
@@ -4,78 +4,78 @@ import Callback from './Callback'
 import Login from './Login'
 import App from '../App'
 
-const DEFAULT_REDIRECT_CALLBACK = () =>
-  window.history.replaceState({}, document.title, window.location.pathname)
+const DEFAULT_REDIRECT_CALLBACK = () => window.history.replaceState({}, document.title, window.location.pathname)
 
-export const Auth0Context = React.createContext()
-export const useAuth0 = () => useContext(Auth0Context)
+export const Auth0Context = React.createContext();
+export const useAuth0 = () => useContext(Auth0Context);
 export const Auth0Provider = ({
   children,
   onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
   ...initOptions
 }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false)
-  const [user, setUser] = useState()
-  const [auth0Client, setAuth0] = useState()
-  const [loading, setLoading] = useState(true)
-  const [popupOpen, setPopupOpen] = useState(false)
-  const [idToken, setIdToken] = useState("")
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [user, setUser] = useState();
+  const [auth0Client, setAuth0] = useState();
+  const [loading, setLoading] = useState(true);
+  const [popupOpen, setPopupOpen] = useState(false);
+  const [idToken, setIdToken] = useState("");
 
   useEffect(() => {
     const initAuth0 = async () => {
-      const auth0FromHook = await createAuth0Client(initOptions)
-      setAuth0(auth0FromHook)
+      const auth0FromHook = await createAuth0Client(initOptions);
+      setAuth0(auth0FromHook);
 
       if (window.location.search.includes("code=")) {
-        const { appState } = await auth0FromHook.handleRedirectCallback()
-        onRedirectCallback(appState)
+        const { appState } = await auth0FromHook.handleRedirectCallback();
+        onRedirectCallback(appState);
       }
 
-      const isAuthenticated = await auth0FromHook.isAuthenticated()
+      const isAuthenticated = await auth0FromHook.isAuthenticated();
 
-      setIsAuthenticated(isAuthenticated)
+      setIsAuthenticated(isAuthenticated);
 
       if (isAuthenticated) {
-        const user = await auth0FromHook.getUser()
-        setUser(user)
-        const idTokenClaims = await auth0FromHook.getIdTokenClaims()
-        setIdToken(idTokenClaims.__raw)
+        const user = await auth0FromHook.getUser();
+        setUser(user);
+        const idTokenClaims = await auth0FromHook.getIdTokenClaims();
+        setIdToken(idTokenClaims.__raw);
       }
 
-      setLoading(false)
-    }
-    initAuth0()
-  }, [])
+      setLoading(false);
+    };
+    initAuth0();
+    // eslint-disable-next-line
+  }, []);
 
   const loginWithPopup = async (params = {}) => {
-    setPopupOpen(true)
+    setPopupOpen(true);
     try {
-      await auth0Client.loginWithPopup(params)
+      await auth0Client.loginWithPopup(params);
     } catch (error) {
-      console.error(error)
+      console.error(error);
     } finally {
-      setPopupOpen(false)
+      setPopupOpen(false);
     }
-    const user = await auth0Client.getUser()
-    setUser(user)
-    setIsAuthenticated(true)
-  }
+    const user = await auth0Client.getUser();
+    setUser(user);
+    setIsAuthenticated(true);
+  };
 
   const handleRedirectCallback = async () => {
-    setLoading(true)
-    const result = await auth0Client.handleRedirectCallback()
-    const user = await auth0Client.getUser()
-    const idTokenClaims = await auth0Client.getIdTokenClaims()
-    setIdToken(idTokenClaims.__raw)
+    setLoading(true);
+    const result = await auth0Client.handleRedirectCallback();
+    const user = await auth0Client.getUser();
+    const idTokenClaims = await auth0Client.getIdTokenClaims();
+    setIdToken(idTokenClaims.__raw);
 
-    setLoading(false)
-    setIsAuthenticated(true)
-    setUser(user)
-    return result
-  }
+    setLoading(false);
+    setIsAuthenticated(true);
+    setUser(user);
+    return result;
+  };
 
   if (loading) {
-    return <Callback />
+    return <Callback />;
   }
   if (!isAuthenticated) {
     return (
@@ -96,7 +96,7 @@ export const Auth0Provider = ({
       >
         <Login />
       </Auth0Context.Provider>
-    )
+    );
   }
 
   return (
@@ -118,5 +118,5 @@ export const Auth0Provider = ({
       {children}
       <App idToken={idToken} />
     </Auth0Context.Provider>
-  )
-}
+  );
+};
