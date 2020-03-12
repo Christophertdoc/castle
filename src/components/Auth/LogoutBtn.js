b6d0a3c9887779a1f53ab10f21f8d4f94b175eca
Christopher Dougherty <christophertdoc@gmail>
2020-03-11 23:06:52 -0400

    cleanup

--- src/components/Auth/LogoutBtn.js
+++ src/components/Auth/LogoutBtn.js
@@ -1,15 +1,10 @@
-import React from "react";
-import { Button } from "react-bootstrap";
+import React from "react"
+import { Button } from "react-bootstrap"
 
 const LogoutBtn = ({ logoutHandler }) => (
-  <Button
-    id="qsLogoutBtn"
-    variant="primary"
-    className="btn-margin logoutBtn"
-    onClick={logoutHandler}
-  >
-    Log Out
-  </Button>
-);
+	<Button variant="primary" onClick={logoutHandler}>
+		Log Out
+	</Button>
+)
 
-export default LogoutBtn;
+export default LogoutBtn
